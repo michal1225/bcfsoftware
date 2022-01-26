8e6c86707692c821d7608a1f159e3a397c4daf87
michal1225 <michal.krawczynski@icloud.com>
2022-01-26 20:25:30 +0100

    poczatek

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
+#include <dirent.h>
 #include <iostream>
+#include <string.h>
+#include <stdio.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello world!" << endl;
+    DIR* dir = opendir(".");
+    if(dir == NULL){
+    return 1;
+    }
+    struct dirent* a;
+    a = readdir(dir);
+    while(a != NULL){
+        cout<< a->d_name <<endl;
+        a = readdir(dir);
+    }
+    closedir(dir);
     return 0;
 }
