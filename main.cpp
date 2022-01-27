f585f444ab456a353b5ae6d25c7bae1eb78da459
michal1225 <michal.krawczynski@icloud.com>
2022-01-27 19:51:10 +0100

    liczy pliki i wiersze

--- main.cpp
+++ main.cpp
@@ -1,21 +1,62 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+#include <thread>
+#include <fstream>
+#include <vector>
 using namespace std;
 using namespace std::filesystem;
 
-void lines(){
+void files(){
     int licznik = 0;
-    for(const directory_entry& entry : recursive_directory_iterator("/home/michal/zadanko/bcfsoftware")) {
+    for(const directory_entry& entry : recursive_directory_iterator("/home/michal/")) {
         bool isNormalFile = is_regular_file(entry);
         auto path = entry.path();
+        string pathString = path.string();
         if(isNormalFile){
-        licznik++;}
-        string filenameString = path.filename().string();
+        licznik++;
         }
+    }
     cout<<"Number of files: "<<licznik<<endl;
 }
+vector<string> lista(){
+    vector<string> arr;
+    for(const directory_entry& entry : recursive_directory_iterator("/home/michal/")) {
+        bool isNormalFile = is_regular_file(entry);
+        auto path = entry.path();
+        string pathString = path.string();
+        if(isNormalFile){
+            arr.push_back(pathString);
+        }
+    }
+    return arr;
+}
+void lines(vector<string> mac){
+    long line = 0;
+    string linia;
+    for (string b: mac){
+        ifstream f;
+        f.open(b);
+        if(!f.is_open()){
+            break;
+            }
+        else{
+            while(getline(f, linia)){
+                line++;
+                }
+            f.close();
+            }
+    }
+    cout<<"Number of lines in files: "<<line<<endl;
+
+}
+
 int main() {
-    lines();
+    thread pliki(files);
+    thread maci(lista);
+    vector<string> macierz = maci.join();
+    long c = lines(macierz);
+    cout<<c;
+
     return 0;
 }
