c4802dfc308a25b2d45cbee466355755d0ed7e95
michal1225 <michal.krawczynski@icloud.com>
2022-01-27 15:18:52 +0100

    Number of files

--- main.cpp
+++ main.cpp
@@ -3,20 +3,19 @@
 #include <filesystem>
 using namespace std;
 using namespace std::filesystem;
-int main() {
+
+void lines(){
+    int licznik = 0;
     for(const directory_entry& entry : recursive_directory_iterator("/home/michal/zadanko/bcfsoftware")) {
-
         bool isNormalFile = is_regular_file(entry);
-        bool isDirectory = is_directory(entry);
         auto path = entry.path();
-
-        string pathString = path.string();
-        cout << pathString << endl;
-
+        if(isNormalFile){
+        licznik++;}
         string filenameString = path.filename().string();
-
-        string extensionString = path.extension().string();
-
-    }
+        }
+    cout<<"Number of files: "<<licznik<<endl;
+}
+int main() {
+    lines();
     return 0;
 }
